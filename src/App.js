55e98e64ce00c04dfb341fd8473def7fbe9d736b
shakerbill <shakerbillo@yahoo.com>
2023-06-28 22:20:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import classNames from 'classnames';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    const [todo, setTodo] = useState('');
+    const [tasks, setTasks] = useState([]);
+
+    const handleTodoChange = (e) => {
+        setTodo(e.target.value);
+    }
+
+    const handleAddClick = () => {
+        if (todo === '') {
+            return;
+        }
+
+        setTasks([...tasks, { name: todo, crossed: false}]);
+        setTodo('');
+    }
+
+    const handleTaskClick = (index) => {
+        const newTasks = [...tasks];
+        newTasks[index].crossed = !newTasks[index].crossed; 
+        setTasks(newTasks);
+
+    }
+
+    let remaining = 0;
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].crossed === false) {
+            remaining = remaining + 1;
+        }
+    }
+
+    return (
+        <>
+            <div>
+                <h2>
+                    Todo List
+                </h2>
+                <input type="text" name='list-item' value={todo} onChange={handleTodoChange} />
+                <button onClick={handleAddClick}>Add</button>
+                <div className="task-counter">
+                    {remaining} remaining out of {tasks.length} tasks
+                </div>
+
+                <ul>
+                    {tasks.map((task, index) => {
+                        const handleClick = () => handleTaskClick(index);
+                        return <li  className={classNames({'is-done': task.crossed})} key={index} onClick={(handleClick)}>{task.name}</li>
+                    })}
+                </ul>
+             
+            </div>
+            <style>{`
+            .is-done {
+                text-decoration: line-through;
+            }
+        `}</style>
+        </>
+    )
 }
-
-export default App;
